63fb8d05e8d433587a7e95e67155768d62b0fdbe
Fabrice Liut <liut.fabrice@gmail.com>
2026-04-24 09:48:51 +0000

    feat: créer a-team-open.html + nav + revert workshops

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         main:             resolve(__dirname, 'index.html'),
         manifesto:        resolve(__dirname, 'manifesto.html'),
         'a-team-workshops': resolve(__dirname, 'a-team-workshops.html'),
+        'a-team-open':    resolve(__dirname, 'a-team-open.html'),
       },
     },
   },
